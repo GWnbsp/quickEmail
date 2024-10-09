a355d849dc8e45ac995d92863edd91f1b218f93f
gaowei <gaowei@relaxcloud.cn>
2024-10-09 17:57:04 +0800

    Add initial project files and configurations

---
+++ config/site.ts
@@ -0,0 +1,17 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "quickEmail",
+  description: "快捷邮件",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "",
+    github: "",
+    docs: "",
+  },
+}
